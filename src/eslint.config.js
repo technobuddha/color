9013352172d20dcc2702ea3ff0ba8cde481dd3d0
Phil Hill <phil@technobuddha.com>
2026-04-10 04:55:12 -0400

    publish

--- src/eslint.config.js
+++ src/eslint.config.js
@@ -3,8 +3,10 @@
 // 🚨
 // @ts-check
 import { lint } from '@technobuddha/project';
-import { defineConfig } from 'eslint/config';
 
-export default defineConfig([
-  lint({ files: ['**/*.ts'], ignores: ['__tests__/**/*', '@types/**/*'], typescript: true }),
-]);
+export default lint({
+  files: ['**/*.ts'],
+  ignores: ['__tests__/**/*', '@types/**/*'],
+  platform: 'node',
+  typescript: true,
+});
