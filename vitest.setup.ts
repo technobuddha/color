5f724f6dba83ac1a6b6f95055c7d40ed95cd5c46
Phil Hill <phil@technobuddha.com>
2026-04-24 15:35:45 -0400

    husky

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -30,6 +30,10 @@ if (import.meta.env.MODE === 'full') {
           });
         }
       }
-    }).catch(() => {});
+    }).catch((error) => {
+      if(error?.code !== 'ENOENT') {
+        throw error;
+      }
+    });
   });
 }
